15a1b0f40f0c6241b1926896fd3bf7190d1f5fad
vaibhavpawarsdet <vaibhavpawarsdet@gmail.com>
2023-04-23 20:29:34 +0530

    value function created and export to deck

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+export function getValue(card) {
+    let data = card.split("-"); //"4-C" -> ["4", "C"]
+    let value = data[0];
+
+    if (isNaN(value)) { //A J Q K
+        if (value == "A") {
+            return 11;
+        }
+        return 10;
+    }
+    return parseInt(value);
+}
